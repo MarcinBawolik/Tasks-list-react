c6c5cda5d1355f667ceddef0c584b8cda0c75246
Marcin Bawolik <marcinbawolik2000@gmail.com>
2023-01-20 10:23:08 +0100

    hideDoneTasks renamed to hideDone

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const tasks = [
   { id: 1, content: "przejść na Reacta", done: false },
   { id: 2, content: "zjeść obiad", done: true },
 ];
-const hideDoneTasks = false;
+const hideDone = false;
 
 function App() {
   return (
@@ -27,10 +27,10 @@ function App() {
       <Section
         title="Lista zadań"
         body={
-          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
+          <Tasks tasks={tasks} hideDone={hideDone} />
         }
         extraHeaderContent={
-          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
+          <Buttons tasks={tasks} hideDone={hideDone} />
         }
       />
 
