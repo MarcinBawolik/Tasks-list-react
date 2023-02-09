0cc91b61d9ed13d92430206dce0771ed3ddd8c90
Marcin Bawolik <marcinbawolik2000@gmail.com>
2023-02-09 17:40:43 +0100

    added GlobalStyle

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
 html {
   box-sizing: border-box;
 }
@@ -12,3 +15,4 @@ body {
   font-family: 'Dancing Script', cursive;
   font-family: 'Lato', sans-serif;
 }
+`;
