3f83162f061c5ffa53b0e27309a64d3d66ffa905
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-02-23 16:55:18 +0100

    application split into features

---
+++ src/features/tasks/TaskList/index.js
@@ -0,0 +1,33 @@
+import { List, Item, Content, Button } from "./styled";
+
+const TaskList = ({ tasks, hideDone, removeTasks, toggleTaskDone }) => (
+    <List>
+        {tasks.map(task => (
+            <Item
+                key={task.id}
+                hidden={task.done && hideDone}
+            >
+                <Button
+                    toggleDone
+                    onClick={() => toggleTaskDone(task.id)}
+                >
+                    {task.done ? "✔️" : ""}
+                </Button>
+                <Content
+                    done={task.done}
+                >
+                    {task.content}
+                </Content>
+                <Button
+                    remove
+                    onClick={() => removeTasks(task.id)}
+                >
+                    🗑️
+                </Button>
+            </Item>
+        ))}
+    </List>
+
+);
+
+export default TaskList;
