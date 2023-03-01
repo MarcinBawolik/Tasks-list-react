ee5e31ae0a9a9aedd3b96f3a686f28eacc60dac1
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-01 10:10:37 +0100

    added example tasks to tasks list

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,29 +1,38 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, setAllDone, toggleHideDone } from "../tasksSlice";
+import {
+    selectTasks,
+    setAllDone,
+    toggleHideDone,
+    fetchExampleTasks,
+} from "../tasksSlice";
 import { StyledButtons, FunctionalButton } from "./styled";
 
 const Buttons = () => {
     const { tasks, hideDone } = useSelector(selectTasks);
     const dispatch = useDispatch();
     return (
-    <StyledButtons>
-        {tasks.length > 0 && (
-            <>
-                <FunctionalButton
-                    onClick={() => dispatch(toggleHideDone())}
-                >
-                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
-                </FunctionalButton>
-                <FunctionalButton
-                    onClick={() => dispatch(setAllDone())}
-                    disabled={tasks.every(({ done }) => done)}
-                >
-                    Ukończ wszystkie
-                </FunctionalButton>
-            </>
-        )}
-    </StyledButtons>
-)};
+        <StyledButtons>
+            <FunctionalButton onClick={() => dispatch(fetchExampleTasks())}>
+                Pobierz przykładowe zadania
+            </FunctionalButton>
+            {tasks.length > 0 && (
+                <>
+                    <FunctionalButton
+                        onClick={() => dispatch(toggleHideDone())}
+                    >
+                        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                    </FunctionalButton>
+                    <FunctionalButton
+                        onClick={() => dispatch(setAllDone())}
+                        disabled={tasks.every(({ done }) => done)}
+                    >
+                        Ukończ wszystkie
+                    </FunctionalButton>
+                </>
+            )}
+        </StyledButtons>
+    )
+};
 
 export default Buttons;
