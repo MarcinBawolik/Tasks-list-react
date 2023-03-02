67b186300d4bdc318049ab011d79a6fe47980549
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-02 11:03:07 +0100

    added new Roots from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,3 +1,31 @@
-import Tasks from "./features/tasks/Tasks"
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import { HashRouter, Link, Switch, Route, Redirect } from "react-router-dom"
+import AuthorPage from "./features/author/AuthorPage";
 
-export default () => <Tasks />
+export default () => (
+    <HashRouter>
+        <nav>
+            <ul>
+                <li><Link to="/zadania">zadania
+                </Link></li>
+                <li><Link to="/autor">O autorze
+                </Link></li>
+            </ul>
+            <Switch>
+            <Route path="/zadania/:id">
+                    <TaskPage />
+                </Route>
+                <Route path="/zadania">
+                    <TasksPage />
+                </Route>
+                <Route path="/autor">
+                    <AuthorPage />
+                </Route>
+                <Route path="/">
+                    <Redirect to="/zadania" />
+                </Route>
+            </Switch>
+        </nav>
+    </HashRouter>
+);
