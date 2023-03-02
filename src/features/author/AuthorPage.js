05bbfb90e169b948815cd47f133d9dc6df539856
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-02 11:01:55 +0100

    added AuthorPage

---
+++ src/features/author/AuthorPage.js
@@ -0,0 +1,18 @@
+import Container from "../../common/Container"
+import Header from "../../common/Header"
+import Section from "../../common/Section"
+
+
+export default () => (
+<Container>
+      <Header
+        title="O autorze"
+      />
+      <Section
+        title="Marcin Bawolik"
+        body={<>
+        nana
+        </>}
+      />
+      </Container>
+);
