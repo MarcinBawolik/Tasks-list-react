e917ad7b18eddd7b02109f796d1b516da6a7218b
MarcinBawolik <marcinbawolik2000@gmail.com>
2023-03-06 11:39:45 +0100

    added discripiont about Autor

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -4,15 +4,21 @@ import Section from "../../common/Section"
 
 
 export default () => (
-<Container>
-      <Header
-        title="O autorze"
-      />
-      <Section
-        title="Marcin Bawolik"
-        body={<>
-        nana
-        </>}
-      />
-      </Container>
+  <Container>
+    <Header
+      title="O autorze"
+    />
+    <Section
+      title="Marcin Bawolik"
+      body={<>
+        <p>
+          Moją największą pasją jest <strong>piłka nożna</strong> i od niedawna <strong>programowanie</strong>!
+        </p>
+        <p>W piłce nożnej odniosłem wiele sukcesów, między innymi <i>Mistrzostwo Polski u15, czy debiut w Reprezentacji Polski u16</i></p>
+        Z racji tego, że mam dużo wolnego czasu postanowiłem za namową kolegi spróbować swoich sił właśnie w programowaniu.
+        Od samego początku programowanie bardzo mi się spodobało i sprawiłom, że nauce od dłuższego czasu poświęcam każdą wolną chwilę!
+        Mam nadzieję, że kurs pomoże mi dostać pracę i będę mógł w pełni rozwijać swoje zainteresownie.
+      </>}
+    />
+  </Container>
 );
